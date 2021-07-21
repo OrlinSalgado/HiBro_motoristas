e877b34a853a12a9fb77c8b0c5ae0d5f9a95cfaf
OrlinSalgado <60558793+OrlinSalgado@users.noreply.github.com>
2021-07-20 23:31:58 -0500

    agregado seleccion de ordenes

--- motoristas/js/controlador.js
+++ motoristas/js/controlador.js
@@ -1,8 +1,12 @@
 function registrarse(){
     console.log("registrarse");
+    document.getElementById('cuerpo').style.background='linear-gradient(to  bottom, #76259f,#ffffff)';
     document.getElementById('contenido').style.display= "none";
     document.getElementById('contenido2').style.display= "none";
-    document.getElementById('contPrincipal').style.backgroundColor= "white";
+   
+   
+    
+   
     document.getElementById('contenidoRegistrar').style.display= "block";
     document.getElementById('contenidoRegistrar2').style.display= "block";
     document.getElementById('btnRegistro').style.display= "none";
@@ -10,9 +14,22 @@ function registrarse(){
 }
 function ordenes(){
     document.getElementById('bienvenidoMotorista').style.display= "block";
-    document.getElementById('cuerpo').style.backgroundImage = 'url(img/ramas.png)';
+    document.getElementById('imagenLogo').style.display= "none";
+    document.getElementById('navegacion').style.justifyContent= "space-between";
+    document.getElementById('navegacion').innerHTML +=`
+    <div class="tamano flex_container3"><img src="img/cuenta.png"  width="70px" id="imagenCuenta"></div>`;
     document.getElementById('contenido').style.display= "none";
     document.getElementById('contenido2').style.display= "none";
     document.getElementById('btnRegistro').style.display= "none";
-    document.getElementById('contPrincipal').style.borderRadius = '0px';
+   
+    
+}
+function tomarOrden(){
+    console.log('tomar orden');
+}
+function ordenesTomadas(){
+    console.log('ordenes tomadas');
+}
+function ordenesEntregadas(){
+    console.log('ordenes entregadas');
 }
